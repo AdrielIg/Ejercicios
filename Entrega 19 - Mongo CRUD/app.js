6184c998ba6af6bdd4bde96720c5307f96259dca
AdrielIg <adrielignaciogomez@gmail.com>
2021-07-07 18:56:40 -0300

    Created collections

---
+++ Entrega 19 - Mongo CRUD/app.js
@@ -0,0 +1,12 @@
+const obj = [
+  { title: "Overwatch", thumbnail: "link-img", price: 1050 },
+  { title: "Tekken 7", thumbnail: "link-img", price: 4980 },
+  {title: "GTA V", thumbnail: "link-img", price: 2500},
+  {title: "Valorant", thumbnail: "link-img", price: 3500},
+  {title: "Civilization VI", thumbnail: "link-img", price: 1980},
+  {title: "It Takes Two", thumbnail: "link-img", price: 4500},
+  {title: "Titanfall 2", thumbnail: "link-img", price: 3600},
+  {title: "Tomb Rider", thumbnail: "link-img", price: 4200},
+  {title: "Fallout 4", thumbnail: "link-img", price: 2508},
+  {title: "The Witcher", thumbnail: "link-img", price: 4608},
+]
